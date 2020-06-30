d563e27d388e116c7010b6784f45e5aaa374b70c
Jeff Wesner <jeff.wesner@usd.edu>
2020-06-30 15:58:15 -0500

    fish family diversity

---
+++ family_diversity_script.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(rfishbase)
+
+
+family_diversity <- fishbase %>% 
+  group_by(Family) %>% 
+  distinct(Species) %>% 
+  tally(name = "no_fish_species") %>% 
+  arrange(-no_fish_species) %>% 
+  mutate(source = "FishBase")
+
+write.csv(family_diversity, file = "family_diversity.csv")          
