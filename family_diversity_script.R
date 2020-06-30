b9b24fedb43eddf32b413eb50d2d08bd10eecaef
Jeff Wesner <jeff.wesner@usd.edu>
2020-06-30 16:10:26 -0500

    include genus

--- family_diversity_script.R
+++ family_diversity_script.R
@@ -2,11 +2,15 @@ library(tidyverse)
 library(rfishbase)
 
 
+
+
 family_diversity <- fishbase %>% 
   group_by(Family) %>% 
-  distinct(Species) %>% 
+  distinct(Genus,Species) %>% 
   tally(name = "no_fish_species") %>% 
   arrange(-no_fish_species) %>% 
   mutate(source = "FishBase")
 
 write.csv(family_diversity, file = "family_diversity.csv")          
+
+
